3d3ae8e62527ea10c9f8537304a5083501712161
Osvaldo Carrizo <osvaldocarrizo8@gmail.com>
2023-03-16 18:19:36 -0300

    fix 2da pre entrega

--- js/name.js
+++ js/name.js
@@ -1,10 +1,33 @@
 
-alert("Bienvenidos a la Verduleria Tomato!");
+//alert("Bienvenidos a la Verduleria Tomato!");
 
-let name = prompt("Ingresa tu nombre");
+//let name = prompt("Ingresa tu nombre");
 
-while (name.length === 0) {
-  name = prompt("Ingresa tu nombre");
-}
+// while (name.length === 0) {
+//   name = prompt("Ingresa tu nombre");
+// }
 
 
+//-------------carrousel------------//
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});

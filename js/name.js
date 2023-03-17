82e9f153e9fb4dcf2c6f378f6726fd947f08ff30
Osvaldo Carrizo <osvaldocarrizo8@gmail.com>
2023-03-16 21:36:44 -0300

    add name.js

--- js/name.js
+++ js/name.js
@@ -31,3 +31,25 @@ const swiper = new Swiper('.swiper', {
     el: '.swiper-scrollbar',
   },
 });
+
+
+//---------------- cards--------------//
+const productContainer = document.getElementById("productos")
+
+const cardVerduras = [
+  {id:1, name:"Frutas", img:"./img/card1.jpg"},
+  {id:2, name:"Verduras", img:"./img/card2.jpg"},
+  {id:3, name:"Verdeos Frescos", img:"./img/card3.jpg"}
+]
+
+cardVerduras.forEach((cards)=>{
+  const div = document.createElement ("div")
+  div.innerHTML = `
+  <img src="${cards.img}" alt="">
+  <h3>${cards.name}</h3>
+  <button id="">Catalogo</button>
+  `
+  
+  productContainer.appendChild(div)
+})
+
